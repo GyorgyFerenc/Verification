1c9348d8ac9c04901709dd014e80eb7fc5644802
GyorgyFerenc <gyferenc2002@gmail.com>
2024-04-10 18:56:39 +0300

    Added DuplicateTest

--- src/test/java/verification/TestAddAssignment.java
+++ src/test/java/verification/TestAddAssignment.java
@@ -49,24 +49,23 @@ public class TestAddAssignment {
 
 	@Test
 	public void testAddAssignmentStringIdEmptyDescription() {
-		Validator<Student> studentValidator = new StudentValidator();
-		Validator<Tema> temaValidator = new TemaValidator();
-		Validator<Nota> notaValidator = new NotaValidator();
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
 
-		StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "test_students.xml");
-		TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "test_projects.xml");
-		NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "test_grades.xml");
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "test_students.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "test_projects.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "test_grades.xml");
 
 		Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
 
 
 		try{
-
-		fileRepository2.delete("1");
-		fileRepository2.delete("2");
-		assertTrue(service.saveTema("1", "", 10, 2) == 1);
-		assertTrue(service.saveTema("2", "", 13, 4) == 1);
-			assertTrue(false);
+            fileRepository2.delete("1");
+            fileRepository2.delete("2");
+            assertTrue(service.saveTema("1", "", 10, 2) == 1);
+            assertTrue(service.saveTema("2", "", 13, 4) == 1);
+            assertTrue(false);
 		}catch(ValidationException e){
 			assertTrue(true);
 		}
@@ -85,10 +84,10 @@ public class TestAddAssignment {
 		Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
 
 		try{
-		fileRepository2.delete("1");
-		fileRepository2.delete("2");
-		assertTrue(service.saveTema("1", "", 0, 0) == 1);
-		assertTrue(service.saveTema("2", "", 0, 0) == 1);
+            fileRepository2.delete("1");
+            fileRepository2.delete("2");
+            assertTrue(service.saveTema("1", "", 0, 0) == 1);
+            assertTrue(service.saveTema("2", "", 0, 0) == 1);
 			assertTrue(false);
 		}catch(ValidationException e){
 			assertTrue(true);
@@ -108,10 +107,10 @@ public class TestAddAssignment {
 		Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
 
 		try{
-		fileRepository2.delete("1");
-		fileRepository2.delete("2");
-		assertTrue(service.saveTema("1", "test Assignment 1", 0, 0) == 1);
-		assertTrue(service.saveTema("2", "test Assignment 2", 0, 0) == 1);
+            fileRepository2.delete("1");
+            fileRepository2.delete("2");
+            assertTrue(service.saveTema("1", "test Assignment 1", 0, 0) == 1);
+            assertTrue(service.saveTema("2", "test Assignment 2", 0, 0) == 1);
 			assertTrue(false);
 		}catch(ValidationException e){
 			assertTrue(true);
@@ -131,10 +130,10 @@ public class TestAddAssignment {
 		Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
 
 		try{
-		fileRepository2.delete("1");
-		fileRepository2.delete("2");
-		assertTrue(service.saveTema("1", "test Assignment 1", -1, 0) == 1);
-		assertTrue(service.saveTema("2", "test Assignment 2", -1, 0) == 1);
+            fileRepository2.delete("1");
+            fileRepository2.delete("2");
+            assertTrue(service.saveTema("1", "test Assignment 1", -1, 0) == 1);
+            assertTrue(service.saveTema("2", "test Assignment 2", -1, 0) == 1);
 			assertTrue(false);
 		}catch(ValidationException e){
 			assertTrue(true);
@@ -154,10 +153,10 @@ public class TestAddAssignment {
 		Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
 
 		try{
-		fileRepository2.delete("1");
-		fileRepository2.delete("2");
-		assertTrue(service.saveTema("1", "test Assignment 1", 10, -1) == 1);
-		assertTrue(service.saveTema("2", "test Assignment 2", 13, -1) == 1);
+            fileRepository2.delete("1");
+            fileRepository2.delete("2");
+            assertTrue(service.saveTema("1", "test Assignment 1", 10, -1) == 1);
+            assertTrue(service.saveTema("2", "test Assignment 2", 13, -1) == 1);
 			assertTrue(false);
 		}catch(ValidationException e){
 			assertTrue(true);
@@ -177,13 +176,32 @@ public class TestAddAssignment {
 		Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
 
 		try{
-		fileRepository2.delete("1");
-		fileRepository2.delete("2");
-		assertTrue(service.saveTema(null, null, 0, 0) == 1);
-		assertTrue(service.saveTema(null, null, 0, 0) == 1);
+            fileRepository2.delete("2");
+            fileRepository2.delete("1");
+            assertTrue(service.saveTema(null, null, 0, 0) == 1);
+            assertTrue(service.saveTema(null, null, 0, 0) == 1);
 			assertTrue(false);
 		}catch(ValidationException e){
 			assertTrue(true);
 		}
 	}
+
+	@Test
+	public void testAddDuplicate() {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "test_students.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "test_projects.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "test_grades.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+    	// public int saveTema(String id, String descriere, int deadline, int startline) {
+		fileRepository2.delete("1");
+		service.saveTema("1", "test Assignment 1", 10, 2);
+		assertTrue(service.saveTema("1", "test Assignment 1", 10, 2) == 0);
+	}
 }
+
